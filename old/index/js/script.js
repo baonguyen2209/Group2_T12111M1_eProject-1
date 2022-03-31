252c359314e2407ad769faeb5e9af53b07125004
hona97 <phuochoan1999qng@gmail.com>
2022-03-31 19:26:58 +0700

    update

--- old/index/js/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = () =>{
-
-    if(window.scrollY > 80){
-        document.querySelector('.header .header-2').classList.add('active');
-    } else {
-        document.querySelector('.header .header-2').classList.remove('active');
-    }
-
-}
